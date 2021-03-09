46cbe6af1f5b669cd6b87144aefc0b6b8ab30af2
newmancroos <newmancroos@gmail.com>
2021-03-09 17:32:37 -0500

    Paging on button click completed

---
+++ ReactFullCourse/6.react-basis-Paging_PageEnd/src/components/Loading.js
@@ -0,0 +1,9 @@
+export default function Loading(){
+    return(
+        <div className="flex h-screen">
+        <p className="m-auto">
+            <i className="fas fa-circle-notch fa-spin text-5xl text-yellow-400"></i>
+        </p>
+    </div>
+    )
+}
